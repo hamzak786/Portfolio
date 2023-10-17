f2e484b4bb5201e8186e248ada81e0cd1286b97d
hamzak786 <mohdhamzak93@gmail.com>
2023-10-17 18:32:45 +0530

    Add files via upload

---
+++ my web/script.js
@@ -0,0 +1,30 @@
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick= () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections=document.querySelectorAll('section');
+let navbarLinks=document.querySelectorAll('header nav a');
+
+window.onscroll =()=>{
+    sections.forEach(sec=>{
+        let top= window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height= sec.offsetHeight;
+        let id=sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            navbarLinks.forEach((link)=>{
+                links.classLists.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        };
+    });
+    let header =document.querySelector('header')
+    header.classList.toggle('sticky',window.ScrollY >100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('remove');
+};
